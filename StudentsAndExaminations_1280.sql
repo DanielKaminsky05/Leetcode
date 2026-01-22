6c26a494bfd93df276bdb5d9a838f1603152bcee
DanielKaminsky05 <daniel.kaminsky23@gmail.com>
2026-01-22 15:35:12 -0500

    q 1280

---
+++ StudentsAndExaminations_1280.sql
@@ -0,0 +1,1 @@
+Select  Students.student_id,Students.student_name,Subjects.subject_name, count(Examinations.subject_name) as attended_exams from Students cross join Subjects left join Examinations on Examinations.student_id = Students.student_id and Examinations.subject_name = Subjects.subject_name group by Students.student_id, Subjects.subject_name, Students.student_name order by student_id 
